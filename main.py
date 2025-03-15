88c7d02c6ff9d064b4f4c78dc8ea5a73df451d96
arskwe <a03507217@gmail.com>
2025-03-15 13:11:18 +0300

    feat 14

--- main.py
+++ main.py
@@ -6,3 +6,13 @@ load_dotenv()
 TOKEN = os.getenv("TOKEN")
 
 print(TOKEN)
+
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+ 
+app = FastAPI()
+ 
+@app.get("/")
+def read_root():
+    html_content = "<h2>Sheet.Master!</h2>"
+    return HTMLResponse(content=html_content)
