e8715d24cb4baeb610d6ea138d6f75d14381bd2d
Ranel211 <ranel.khalimov.12@mail.ru>
2025-04-13 18:34:12 +0300

    feat

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from gspread import Client, Spreadsheet, Worksheet, service_account
+from gspread import Client
+from sheet_class import Sheet
 from os import getenv
 from dotenv import load_dotenv
 from fastapi import FastAPI, Body
@@ -7,12 +8,7 @@ from fastapi.responses import HTMLResponse
 table_link ='https://docs.google.com/spreadsheets/d/'
 table_id = "1VKkXzFKOL85l8AiTdgk7fqBVAlsFUYw71vQS09ZOYTg"
 
-def get_table_by_key(client, table_key):
-    """Получение таблицы из Google Sheets по id."""
-    return client.open_by_key(table_key)
-
-
-Client = client_init_json()
+Client = Sheet.client_init_json()
 print(Client, 'Клиент')
 print(table_link + table_id, 'Таблица')
 
@@ -20,16 +16,18 @@ load_dotenv()
 
 TOKEN = getenv("TOKEN")
 
-print(TOKEN)
- 
 app = FastAPI()
- 
+
+
 @app.get("/")
 def read_root():
+    """Test method"""
     html_content = "<h2>Sheet.Master!</h2>"
-    table = get_table_by_key(Client, table_id)
-    print(table, 'Таблица')
+    table = Sheet.get_table_by_key(Client, table_id)
+    Sheet.create_row(table, 'fd', [1,2, 2])
     return HTMLResponse(content=html_content)
+
 @app.post('/create')
 async def create_table(name:str = Body(embed=True, min_length=3, max_length=20)):
+    """create table from google sheet"""
     return name
--- sheet_class.py
+++ sheet_class.py
@@ -1,38 +1,43 @@
-from gspread import service_account
+from gspread import service_account, Client, Spreadsheet
 
 class Sheet:
-    def create():
-        pass
+    """Класс для работы с библиотекой gspread"""
+    @staticmethod
+    def create_row(table: Spreadsheet, sheet_name: str, data: list):
+        """Создание записи"""
+        worksheet = table.worksheet(sheet_name)
+        worksheet.append_row(data)
 
+    @staticmethod
     def info_about_sheets():
-        pass
+        """Информация о таблице"""
+        return
 
-    def get_table_but_key():
-        pass
+    @staticmethod
+    def get_table_by_key(client: Client, table_key: str):
+        """получить таблицу c помощью ключа"""
+        return client.open_by_key(table_key)
 
+    @staticmethod
     def client_init_json():
         """Создание клиента для работы с Google Sheets."""
         return service_account(filename='creds.json')
 
-# def get_table_by_id(client: Client, table_url):
-#     """Получение таблицы из Google Sheets по ID таблицы."""
-#     return client.open_by_key(table_url)
-# def test_get_table(table_url: str, table_key: str):
-#     """Тестирование получения таблицы из Google Sheets."""
-#     client = client_init_json()
-#     table = get_table_by_url(client, table_url)
-#     print('Инфо по таблице по ссылке: ', table)
-#     table = get_table_by_id(client, table_key)
-#     print('Инфо по таблице по id: ', table)
+    @staticmethod
+    def get_table_by_id(client: Client, table_url: str):
+        """Получение таблицы из Google Sheets по ID таблицы."""
+        return client.open_by_key(table_url)
 
+    # @staticmethod
+    # def test_get_table(table_url: str, table_key: str):
+    #     """Тестирование получения таблицы из Google Sheets."""
+    #     client = self.client_init_json()
+    #     table = self.get_table_by_url(client, table_url)
+    #     print('Инфо по таблице по ссылке: ', table)
+    #     table = self.get_table_by_id(client, table_key)
+    #     print('Инфо по таблице по id: ', table)
 
-# if __name__ == '__main__':
-#     test_get_table(table_link, table_id)
-#     def create_worksheet(table: Spreadsheet, title: str, rows: int, cols: int):
-#         return table.add_worksheet(title, rows, cols)
-
-# def test_create_worksheet():
-#     client = client_init_json()
-#     table = get_table_by_id(client, table_id)
-#     rez = create_worksheet(table, 'НовыйРабочийЛист', rows=15, cols=10)
-#     print(rez)
+    @staticmethod
+    def create_worksheet(table: Spreadsheet, title: str, rows: int, cols: int):
+        """Create a new worksheet"""
+        return table.add_worksheet(title, rows, cols)
