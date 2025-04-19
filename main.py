dfc19adb6e2cf81f064737893632e31917fafee8
arskwe <a03507217@gmail.com>
2025-04-19 13:47:39 +0300

    feat: #33

--- main.py
+++ main.py
@@ -31,3 +31,10 @@ def read_root():
 async def create_table(name:str = Body(embed=True, min_length=3, max_length=20)):
     """create table from google sheet"""
     return name
+
+@app.get("/health")
+def read_root():
+    """Test method"""
+    html_content = "<h2>good!</h2>"
+    return HTMLResponse(content=html_content)
+
